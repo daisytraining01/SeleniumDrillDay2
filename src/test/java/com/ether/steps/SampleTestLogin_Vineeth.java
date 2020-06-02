3080081a36ddba9625210e6c90427ffe575865b4
Syed Naser Uddin <syednaseru@maveric-systems.com>
2020-06-02 20:13:35 +0530

    step definition

--- src/test/java/com/ether/steps/SampleTestLogin_Vineeth.java
+++ src/test/java/com/ether/steps/SampleTestLogin_Vineeth.java
@@ -19,7 +19,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class SampleTestLogin {
+public class SampleTestLogin_Vineeth {
 	TestUtil util = new TestUtil();
 	LoginPageLocators locator = new LoginPageLocators();
 	HomePageLocators locatorHome=new HomePageLocators();
